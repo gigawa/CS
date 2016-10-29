a39dd413ce9254c4f9c132c991481545d6fb120e
Grant Hooks <grant.hooks@ymail.com>
2016-10-29 16:56:44 -0700

    Count

---
+++ PA04/CountingSort.h
@@ -0,0 +1,12 @@
+#ifndef COUNTING_SORT_
+#define COUNTING_SORT_
+
+template <class ItemType>
+class CountingSort {
+public:
+	CountingSort(ItemType theArray[], int n, int & c, int & s);
+private:
+};
+
+#include "CountingSort.cpp"
+#endif
